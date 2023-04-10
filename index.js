0be6d36c717dedf2345da605cbf3d681a5deba79
Bruno Disliler <bruno_disliler@outlook.com>
2023-04-10 14:51:57 -0300

    Criando variáveis de embiente e conectando ao MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 // Forma de ler JSON / Middlewares
 app.use(
@@ -15,5 +18,21 @@ app.get("/", (req, res) => {
 });
 
 // Porta
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
+const DB_PORT = process.env.DB_PORT;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+mongoose
+	.connect(
+		`mongodb+srv://${DB_USER}:${DB_PASSWORD}@api2.ugvv994.mongodb.net/?retryWrites=true&w=majority`
+	)
+	.then(() => {
+		console.log("Conectamos ao MongoDB");
+		app.listen(DB_PORT);
+	})
+	.catch((err) => console.log(err));
+
+// brunodislilerdev
+// qh5eG1BvT8axqd8P;
+// mongodb+srv://brunodislilerdev:qh5eG1BvT8axqd8P@api2.ugvv994.mongodb.net/?retryWrites=true&w=majority
+// Criando variáveis de ambiente e conectar MongoDB
